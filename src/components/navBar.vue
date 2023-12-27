4538a8e8b07127f0aa1f650814731f49654dba20
devbitan <jquirama96@gmail.com>
2023-12-27 13:47:04 -0500

    first

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -17,7 +17,7 @@ const toggleTheme = () => {
 <template lang="pug">
 nav#desktop-nav
   .logo 
-    div.logo-name Dev Bitann
+    div.logo-name Dev Bitan
     div.toggle-theme(@click='toggleTheme()')
       button.buttonChange Theme
     //- button.buttonChange Language
@@ -33,7 +33,6 @@ nav#desktop-nav
         a(href='#contact') Contact
 nav#hamburger-nav
   .logo-name Dev Bitan
-    .logo-name Dev Bitann
     .toggle-theme(@click='toggleTheme()')
       button.buttonChange Theme
     //- button.buttonChange Language
